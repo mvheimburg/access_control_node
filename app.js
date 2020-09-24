9d60760d16d3d8b3e36dcd2ccccba26e7eb39407
Martin von Heimburg <Martin.Heimburg@sintef.no>
2020-09-24 14:02:09 +0200

    fixing web/app mistake

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'web')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 
 module.exports = app;
