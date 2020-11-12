cb1e891890e3428ff096e68f719837d27b87439f
Martin von Heimburg <Martin.Heimburg@sintef.no>
2020-11-12 21:34:18 +0100

    https

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "fa05a80b77e31336f897d62994ebec27",
-"/": "fa05a80b77e31336f897d62994ebec27",
-"main.dart.js": "c1a84d770ffa568c43de76dc5d79fe44",
+"index.html": "81d75d90a6e1534115d338deddd5fcbc",
+"/": "81d75d90a6e1534115d338deddd5fcbc",
+"main.dart.js": "266d52fb78655c0d59a1be2c239560cf",
 "manifest.json": "03c8ab7975bcd11ff792b06572e67922"
 };
 
